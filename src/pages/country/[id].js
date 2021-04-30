d051b5254fe2b6135c13028c4b949ae00242ed32
ngocduyhhtb <duy191203607@st.utc.edu.vn>
2021-04-30 16:26:20 +0700

    fix dynamic route

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -104,7 +104,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: true,
+        fallback: false,
     };
 };
 export const getStaticProps = async ({params}) => {
