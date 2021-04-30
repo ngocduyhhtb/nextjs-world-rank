0c7d0c590504f2c9f4341502f8d89c508855c860
ngocduyhhtb <duy191203607@st.utc.edu.vn>
2021-04-30 16:24:37 +0700

    update dynamic router

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -104,7 +104,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     };
 };
 export const getStaticProps = async ({params}) => {
